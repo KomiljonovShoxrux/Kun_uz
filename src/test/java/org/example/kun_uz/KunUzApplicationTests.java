fc12da5985f57ac871cf3afb182e03cca9151793
Shoxrux <komiljonovshoxrux19@gmail.com>
2025-03-03 11:12:00 +0500

    add project

---
+++ src/test/java/org/example/kun_uz/KunUzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kun_uz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
